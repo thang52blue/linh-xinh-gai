540a74f463d720da3ae78c76b266ca04ccd5dd25
5chuc <>
2025-06-17 21:45:53 +0700

    init

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+	introTitle: "Nè Linh ơi~ ☁️",
+	introDesc: `Trời thì nhiều mây,
+    Em thì nhiều nét đáng yêu 😚
+    Không biết có ai nói chưa,
+    Nhưng nhìn em là auto vui cả ngày luôn đó 🩵`,
+	btnIntro: "Hí hí bấm thử nè >//<",
+	title: "Chào em người vừa xinh vừa dễ thương nhất khu trọ 😆",
+	desc: "Nếu được chọn crush lại từ đầu… thì anh vẫn chọn em á 😳",
+	btnYes: "Trời ơi cute quá hà 🥺",
+	btnNo: "Không nha, đừng dụ em 😝",
+	question:
+		"Cho anh xin một đặc quyền nhỏ được hong… ví dụ như là làm bạn của em chẳng hạn 😌",
+	btnReply: "Gửi cho anh nè 💌",
+	reply: "Dạ… cũng muốn thử cho anh cơ hội á 🥰",
+	mess: "Thật hông đó 😳 Anh đang cười tủm tỉm luôn nè 🫣",
+	messDesc: "Mai nếu em thèm nước cam… cứ nói nhỏ, anh sẽ teleport liền 🍊",
+	btnAccept: "Okiiii luôn 💖",
+	messLink: "https://www.facebook.com/lethang52blue" 
+};
